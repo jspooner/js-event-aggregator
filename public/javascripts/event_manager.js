21b7e007aca5115721643db1def53167fbbde377
Jonathan Spooner <jspooner@gmail.com>
2014-07-31 13:41:07 -0700

    added channel to publish and subscribe

--- public/javascripts/event_manager.js
+++ public/javascripts/event_manager.js
@@ -18,6 +18,9 @@
     this._subscriptions = [];
     this._nextId        = 0;
   }
+  Message.prototype.unsubscribe = function(id) {
+    this._subscriptions[id] = undefined;
+  },
   Message.prototype.subscribe = function(callback) {
     var subscription = new Subscription(this._nextId++, callback);
     this._subscriptions[subscription.id] = subscription;
@@ -31,6 +34,14 @@
       }
     }
   };
+  //
+  // Channel
+  //
+  var Channel = function(channelName) {
+    console.log("new Channel", channelName);
+    this._messages      = {}
+    this._channel       = channelName;
+  }
   /**
   * EventManager 
   * The EventManager is an event aggregator with cross iFrame support.
@@ -43,18 +54,15 @@
   *    });
   *    console.log('token for unsubscribing', token);
   *    em.publish('click', { 'foo' : 'bar' });
-  *
-  *
   **/
   var EventManager = function() {
-    this._messages = {}
+    this._channels = {}                                                         // { 'ch1': new Message() }
     console.log("new EventManager", window.location.href);
     this._otherWindow = parent;
     this._targetOrigin = "*";
-    if (location.ancestorOrigins.length > 0) {
+    if (location.ancestorOrigins && location.ancestorOrigins.length > 0) {
       this._targetOrigin = location.ancestorOrigins[0];
-    }
-    
+    }    
     window.addEventListener("message", function(event) { // can we move this into a private method? ex: _handlePostMessage
       var message = event['data'];
       console.log("EventManager handeling postMesage", message);
@@ -66,32 +74,43 @@
   }
   /**
   * Subscribe to an event from this or another window
+  * This will notify the current window before notifying the other window.
   *
   * @method subscribe
   * @param {String} message
   * @param {Function} callback
   * @return {String} Returns a token that is used to unsubscribe
   */
-  EventManager.prototype.subscribe = function(message, callback) {
-    console.log("EventManager.subscribe", message, this._messages[message]);
-    var message = this._messages[message] || (this._messages[message] = new Message(message));
+  EventManager.prototype.subscribe = function(channel, message, callback) {
+    // console.log("EventManager.subscribe", channel, message);
+    var channel = this._channels[channel] || (this._channels[channel] = new Channel(channel))         // has hash of of channels
+    var message = channel._messages[message] || (channel._messages[message] = new Message(message));  // each channel has a hash of messages
     return message.subscribe(callback);
   };
   /**
-  * Unsubscribe from an event from this or another window
+  * Unsubscribe from an event from this or another window.
+  * 
   *
   * @method unsubscribe
   * @param {String} message
   * @param {String} token
   * @return {Boolean} 
   */
-  EventManager.prototype.unsubscribe = function(message, token) {
-  };
-  EventManager.prototype.publish = function(message, payload) {
+  EventManager.prototype.unsubscribe = function(channel, message, token) {
+    console.log("-------------", channel, message, token)
     if(this._messages[message]) {
-      ((this._messages[message])).notify(payload);
+      this._messages[message].unsubscribe(token);
     }
-    this._otherWindow.postMessage(message, this._targetOrigin);
+  };
+  EventManager.prototype.publish = function(channel, message, payload) {
+    console.log('publish', channel, message);
+    if (this._channels[channel] && this._channels[channel]._messages[message]) {
+      (this._channels[channel]._messages[message]).notify(payload);
+    }
+    // if(this._messages[message]) {
+    //   ((this._messages[message])).notify(payload);
+    // }
+    // this._otherWindow.postMessage(message, this._targetOrigin);
   };
   
   
